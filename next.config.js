c4de219487bfac3e4b5be42c157439383074f326
SivanT74 <sivantillakoy@gmail.com>
2024-06-20 00:24:22 +0200

    commit

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unopimized: true,
+  }
 }
 
 module.exports = nextConfig
-
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  assetPrefix: isProd ? '/<repository>/' : '',
-  images: {
-    loader: 'imgix',
-    path: '',
-  },
-  basePath: isProd ? '/<repository>' : '',
-}
