c00a41cef08f4bef8665ca6ef5c8c02e25a9847a
SivanT74 <sivantillakoy@gmail.com>
2024-06-20 10:06:32 +0200

    varcel build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out', // Default is .next, but 'out' is common for export
+  distDir: 'out',
   images: {
     unoptimized: true,
   },
-  basePath: '', // Set this if your GitHub Pages project is in a subdirectory
-  assetPrefix: './', // Ensure relative paths
+  basePath: '', 
+  assetPrefix: './', 
 }
 
 module.exports = nextConfig;
