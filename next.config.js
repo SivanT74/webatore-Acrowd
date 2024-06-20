11f0d892defbfe887c4c0d0a5d816fdf6a5cb6ff
SivanT74 <sivantillakoy@gmail.com>
2024-06-20 11:53:45 +0200

    remove env commit

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '', // Set this if your project is served from a subdirectory, otherwise leave as ''
-  assetPrefix: '', // Set this to the URL where your assets are hosted, otherwise leave as ''
-  env: {
-    NEXT_PUBLIC_CONSUMER_KEY: process.env.NEXT_PUBLIC_CONSUMER_KEY,
-    NEXT_PUBLIC_CONSUMER_SECRET: process.env.NEXT_PUBLIC_CONSUMER_SECRET,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
+  basePath: '', 
+  assetPrefix: '', 
 };
 
 module.exports = nextConfig;
