aab984612c4ac5792532d102d791dfe17777fe44
SivanT74 <sivantillakoy@gmail.com>
2024-06-23 21:23:13 +0200

    hope

--- components/ImageComponent.js
+++ components/ImageComponent.js
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const ImageComponent = ({ src, alt }) => {
   if (isProduction) {
-    return <img src={src} alt={alt} style={{ width: '100%', height: 'auto' }} />;
+    return <img src={src} alt={alt} style={{ width: '100%', height: '130px' }} />;
   } else {
     return (
       <Image
