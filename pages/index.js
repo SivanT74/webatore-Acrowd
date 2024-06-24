1f56e915843220a7baf91efef47f7762eb6f7d9f
SivanT74 <sivantillakoy@gmail.com>
2024-06-24 10:04:55 +0200

    meta 4 index

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,20 @@ const HomePage = () => {
       <Head>
         <title>Welcome to Our Shop</title>
         <meta name="description" content="Discover our wide range of products and amazing deals. Shop now at our online store." />
-        <meta name="keywords" content="shop, online store, products, deals" />
+
+        <meta property="og:url" content="https://webstore-acrowd.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Welcome to Our Shop" />
+        <meta property="og:description" content="Discover our wide range of products and amazing deals. Shop now at our online store." />
+        <meta property="og:image" content="/images/placeholder.webp" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="webstore-acrowd.vercel.app" />
+        <meta property="twitter:url" content="https://webstore-acrowd.vercel.app/" />
+        <meta name="twitter:title" content="Welcome to Our Shop" />
+        <meta name="twitter:description" content="Discover our wide range of products and amazing deals. Shop now at our online store." />
+        <meta name="twitter:image" content="/images/placeholder.webp" />
+
       </Head>
       <h1>Welcome to Our Shop!</h1>
       <p>Discover our wide range of products and amazing deals.</p>
